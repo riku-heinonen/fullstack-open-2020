98a058eff38b1765b40fb7cb1ed1d32cb81372e2
Riku <riku.s.heinonen@aalto.fi>
2020-09-05 20:36:14 +0300

    Complete 1.8

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -9,19 +9,32 @@ const Button = ({ handleClick, text }) => (
 
 const Display = ({ name, value }) => <div>{name} {value}</div>
 
-const App = () => {
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
-
-  const calculateAverage = (good, neutral, bad) => {
+const Statistics = ({ good, neutral, bad }) => {
+  const calculateAverage = () => {
     const total = good + neutral + bad
     return total === 0 ? 0 : (good - bad) / total
   }
 
-  const calculatePositiveRatio = (good, total) => {
+  const calculatePositiveRatio = () => {
+    const total = good + neutral + bad
     return total === 0 ? 0 : good / total * 100
   }
+  return (
+    <div>
+      <Display name="good" value={good}></Display>
+      <Display name="neutral" value={neutral}></Display>
+      <Display name="bad" value={bad}></Display>
+      <Display name="all" value={good + neutral + bad}></Display>
+      <Display name="average" value={calculateAverage()}></Display>
+      <Display name="positive" value={`${calculatePositiveRatio()} %`}></Display>
+    </div>
+  )
+}
+
+const App = () => {
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
 
   return (
     <div>
@@ -30,12 +43,7 @@ const App = () => {
       <Button handleClick={() => setNeutral(neutral + 1)} text="neutral"></Button>
       <Button handleClick={() => setBad(bad + 1)} text="bad"></Button>
       <h2>Statistics</h2>
-      <Display name="good" value={good}></Display>
-      <Display name="neutral" value={neutral}></Display>
-      <Display name="bad" value={bad}></Display>
-      <Display name="all" value={good + neutral + bad}></Display>
-      <Display name="average" value={calculateAverage(good, neutral, bad)}></Display>
-      <Display name="positive" value={`${calculatePositiveRatio(good, good + neutral + bad)} %`}></Display>
+      <Statistics good={good} neutral={neutral} bad={bad}></Statistics>
     </div>
   )
 }
