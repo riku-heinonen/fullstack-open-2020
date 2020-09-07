2e09fc4fcf1c94af45e0141a8147ce3fcafcdad0
Riku Heinonen <riku.s.heinonen@gmail.com>
2020-09-07 21:18:37 +0300

    Complete part 3a

---
+++ part3/phonebook/server.js
@@ -0,0 +1,91 @@
+const express = require('express')
+const morgan = require('morgan')
+const app = express()
+
+app.use(express.json())
+
+morgan.token('postBody', (req) => JSON.stringify(req.body))
+
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :postBody'))
+
+let persons = [
+	{
+		name: 'Arto Hellas',
+		number: '040-123456',
+		id: 1
+	},
+	{
+		name: 'Ada Lovelace',
+		number: '39-44-5323523',
+		id: 2
+	},
+	{
+		name: 'Dan Abramov',
+		number: '12-43-234345',
+		id: 3
+	},
+	{
+		name: 'Mary Poppendieck',
+		number: '39-23-6423122',
+		id: 4
+	}
+]
+
+app.get('/api/persons', (req, res) => {
+	res.json(persons)
+})
+
+app.get('/info', (req, res) => {
+	const content = `
+    <p>Phonebook has information for ${persons.length} people</p>
+    <p>${new Date()}</p>
+  `
+	res.send(content)
+})
+
+app.get('/api/persons/:id', (req, res) => {
+	const id = Number(req.params.id)
+	const person = persons.find((person) => person.id === id)
+	if (person) {
+		res.json(person)
+	} else {
+		res.status(404).end()
+	}
+})
+
+app.delete('/api/persons/:id', (req, res) => {
+	const id = Number(req.params.id)
+	persons = persons.filter((person) => person.id !== id)
+	res.status(204).end()
+})
+
+app.post('/api/persons', (req, res) => {
+	const body = req.body
+
+	if (!(body.name && body.number)) {
+		return res.status(400).json({
+			error: 'content missing'
+		})
+	}
+
+	if (persons.find((person) => person.name == body.name)) {
+		return res.status(400).json({
+			error: 'name must be unique'
+		})
+	}
+
+	const person = {
+		name: body.name,
+		number: body.number,
+		id: Math.floor(Math.random() * 100000)
+	}
+
+	persons.push(person)
+
+	res.json(person)
+})
+
+const PORT = 3001
+app.listen(PORT, () => {
+	console.log(`Server running on port ${PORT}`)
+})
