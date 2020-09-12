3ab5112fb552cdc1bd1e7885c2bd7d8d1224dd4e
Riku <riku.s.heinonen@gmail.com>
2020-09-13 02:21:52 +0300

    Return user id in login response

--- part4/bloglist/controllers/login.js
+++ part4/bloglist/controllers/login.js
@@ -8,22 +8,26 @@ loginRouter.post('/', async (request, response) => {
 
   const user = await User.findOne({ username: body.username })
   const passwordCorrect =
-    user === null ? false : await bcrypt.compare(body.password, user.passwordHash)
+    user === null
+      ? false
+      : await bcrypt.compare(body.password, user.passwordHash)
 
   if (!(user && passwordCorrect)) {
     return response.status(401).json({
-      error: 'invalid username or password'
+      error: 'invalid username or password',
     })
   }
 
   const userForToken = {
     username: user.username,
-    id: user._id
+    id: user._id,
   }
 
   const token = jwt.sign(userForToken, process.env.SECRET)
 
-  response.status(200).send({ token, username: user.username, name: user.name })
+  response
+    .status(200)
+    .send({ token, username: user.username, name: user.name, id: user.id })
 })
 
 module.exports = loginRouter
