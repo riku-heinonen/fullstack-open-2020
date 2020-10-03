ded32407ea9075ea4e944f14586df729323785c8
Riku Heinonen <riku.s.heinonen@gmail.com>
2020-10-03 13:45:28 +0300

    Add library frontend

---
+++ part8/library/frontend/src/components/Authors.js
@@ -0,0 +1,35 @@
+import { ALL_AUTHORS } from '../queries'
+import React from 'react'
+import { useQuery } from '@apollo/client'
+
+const Authors = () => {
+  const result = useQuery(ALL_AUTHORS)
+  const authors = result.data
+  return (
+    <>
+      {!result.loading && authors && (
+        <div>
+          <h2>authors</h2>
+          <table>
+            <tbody>
+              <tr>
+                <th></th>
+                <th>born</th>
+                <th>books</th>
+              </tr>
+              {authors.map((a) => (
+                <tr key={a.name}>
+                  <td>{a.name}</td>
+                  <td>{a.born}</td>
+                  <td>{a.bookCount}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      )}
+    </>
+  )
+}
+
+export default Authors
